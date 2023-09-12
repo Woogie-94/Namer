926bdd51db499094a6bd08228e01f6afcab273e3
Woogie-94 <dmsdmr9411@gmail.com>
2023-09-13 02:00:51 +0900

    feat: styled-components를 Next에 사용할 수 있도록 셋팅

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import StyledComponentsRegistry from "@/lib/styledRegistry";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -10,7 +12,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="kr">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
     </html>
   );
 }

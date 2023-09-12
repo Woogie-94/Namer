e13ae551bed06ab9077e392ab4697873cab654a2
Woogie-94 <dmsdmr9411@gmail.com>
2023-09-13 03:28:50 +0900

    feat: Typo system 추가

---
+++ styles/typo.ts
@@ -0,0 +1,54 @@
+import { css } from "styled-components";
+
+const typo_base = css`
+  font-family: "baseFont";
+  font-style: normal;
+`;
+
+export const typo_12_reqular = css`
+  ${typo_base};
+  font-size: 12px;
+  font-weight: normal;
+`;
+
+export const typo_12_bold = css`
+  ${typo_base};
+  font-size: 12px;
+  font-weight: bold;
+`;
+
+export const typo_16_reqular = css`
+  ${typo_base};
+  font-size: 16px;
+  font-weight: normal;
+`;
+
+export const typo_16_bold = css`
+  ${typo_base};
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+export const typo_18_reqular = css`
+  ${typo_base};
+  font-size: 18px;
+  font-weight: normal;
+`;
+
+export const typo_18_bold = css`
+  ${typo_base};
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+export const typo_32_reqular = css`
+  ${typo_base};
+  font-size: 32px;
+  font-weight: normal;
+`;
+
+export const typo_32_bold = css`
+  ${typo_base};
+  font-size: 32px;
+  font-weight: bold;
+`;

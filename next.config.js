51463f6abb88476de1c93f30d5acd5562c32449f
Woogie-94 <dmsdmr9411@gmail.com>
2023-09-13 02:05:52 +0900

    chore: font 확장자 import 가능하도록 url-loader를 사용해 config 설정 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(woff|woff2|ttf)$/,
+      use: [{ loader: "url-loader" }],
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;

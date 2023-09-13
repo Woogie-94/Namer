b1e82384c935a953057f25ff29df935199561cc9
Woogie-94 <dmsdmr9411@gmail.com>
2023-09-13 17:01:52 +0900

    feat: 옵션 Controller 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,56 @@
-export default function Home() {
-  return <></>;
+"use client";
+
+import { useState } from "react";
+import { styled } from "styled-components";
+
+import Controller from "@/components/main/Controller";
+
+export const enum NamingCase {
+  CamelCase = "camelCase",
+  SnakeCase = "snake_case",
+  PascalCase = "PascalCase",
+  KebabCase = "kebab-case",
 }
+
+const Main = () => {
+  const [isVariable, setIsVariable] = useState(true);
+  const [namingCase, setNamingCase] = useState<NamingCase>(NamingCase.CamelCase);
+
+  const handleIdentifierTypeChange = (value: boolean) => {
+    setIsVariable(value);
+  };
+  const handleNamingCaseChange = (value: NamingCase) => {
+    setNamingCase(value);
+  };
+
+  return (
+    <Wrapper>
+      <Inner>
+        <Controller
+          isVariable={isVariable}
+          namingCase={namingCase}
+          onIdentifierTypeChange={handleIdentifierTypeChange}
+          onNamingCaseChange={handleNamingCaseChange}
+        />
+      </Inner>
+    </Wrapper>
+  );
+};
+
+export default Main;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  padding-top: 160px;
+  background-color: #35363a;
+`;
+
+const Inner = styled.div`
+  width: 100%;
+  max-width: 592px;
+  padding: 0 16px;
+`;
---
+++ components/main/Controller.tsx
@@ -0,0 +1,73 @@
+import { styled } from "styled-components";
+
+import Button from "../common/Button";
+
+import { NamingCase } from "@/app/page";
+
+interface Props {
+  isVariable: boolean;
+  namingCase: NamingCase;
+  onIdentifierTypeChange: (value: boolean) => void;
+  onNamingCaseChange: (value: NamingCase) => void;
+}
+
+const Controller = ({ isVariable, namingCase, onIdentifierTypeChange, onNamingCaseChange }: Props) => {
+  return (
+    <>
+      <ControllerWrapper>
+        <ButtonWrapper>
+          <Button
+            type={isVariable ? "primary" : "secondary"}
+            size="small"
+            label="Variable"
+            onClick={() => onIdentifierTypeChange(true)}
+          />
+          <Button
+            type={isVariable ? "secondary" : "primary"}
+            size="small"
+            label="Function"
+            onClick={() => onIdentifierTypeChange(false)}
+          />
+        </ButtonWrapper>
+        <ButtonWrapper>
+          <Button
+            type={namingCase === NamingCase.KebabCase ? "primary" : "secondary"}
+            size="small"
+            label="Kebab"
+            onClick={() => onNamingCaseChange(NamingCase.KebabCase)}
+          />
+          <Button
+            type={namingCase === NamingCase.PascalCase ? "primary" : "secondary"}
+            size="small"
+            label="Pascal"
+            onClick={() => onNamingCaseChange(NamingCase.PascalCase)}
+          />
+          <Button
+            type={namingCase === NamingCase.SnakeCase ? "primary" : "secondary"}
+            size="small"
+            label="Snake"
+            onClick={() => onNamingCaseChange(NamingCase.SnakeCase)}
+          />
+          <Button
+            type={namingCase === NamingCase.CamelCase ? "primary" : "secondary"}
+            size="small"
+            label="Camel"
+            onClick={() => onNamingCaseChange(NamingCase.CamelCase)}
+          />
+        </ButtonWrapper>
+      </ControllerWrapper>
+    </>
+  );
+};
+
+export default Controller;
+
+const ControllerWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 8px;
+`;
+const ButtonWrapper = styled.div`
+  display: flex;
+  gap: 8px;
+`;

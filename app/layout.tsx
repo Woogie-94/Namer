017097085525403a17825fe310a5340ee9051fca
Woogie-94 <dmsdmr9411@gmail.com>
2023-09-15 16:32:33 +0900

    feat: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,9 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Namer",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
